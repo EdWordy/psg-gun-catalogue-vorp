672d6428cb4ca1be62a4cd9237941f7945a4f4d3
EdWordy_v2.0 <taylorjordanedwards@gmail.com>
2024-06-24 13:23:39 -0600

    Update 6
    -Fix ammo buying

--- server.lua
+++ server.lua
@@ -1,28 +1,28 @@
 local weapons = {
-    [1] = { ['weapon'] = 'WEAPON_REVOLVER_CATTLEMAN', ["PRICE"] = 10, ['label'] = 'Cattleman Revolver', ['AMMOlabel'] = 'ammorevolvernormal', ["AMMOPRICE"] = 1, ["AMMOCOUNT"] = 100 },
-    [2] = { ['weapon'] = 'WEAPON_REVOLVER_DOUBLEACTION', ["PRICE"] = 20, ['label'] = 'Double-Action Revolver', ['AMMOlabel'] = 'ammorevolvernormal', ["AMMOPRICE"] = 1, ["AMMOCOUNT"] = 100 },
-    [3] = { ['weapon'] = 'WEAPON_REVOLVER_LEMAT', ["PRICE"] = 55, ['label'] = 'LeMat Revolver', ['AMMOlabel'] = 'ammorevolvernormal', ["AMMOPRICE"] = 1, ["AMMOCOUNT"] = 100 },
-    [4] = { ['weapon'] = 'WEAPON_REVOLVER_SCHOFIELD', ["PRICE"] = 30, ['label'] = 'Schofield Revolver', ['AMMOlabel'] = 'ammorevolvernormal', ["AMMOPRICE"] = 1, ["AMMOCOUNT"] = 100 },
-    [5] = { ['weapon'] = 'WEAPON_PISTOL_VOLCANIC', ["PRICE"] = 35, ['label'] = 'Volcanic Pistol', ['AMMOlabel'] = 'ammopistolnormal', ["AMMOPRICE"] = 1, ["AMMOCOUNT"] = 100 },
-    [6] = { ['weapon'] = 'WEAPON_PISTOL_SEMIAUTO', ["PRICE"] = 120, ['label'] = 'Semi-Auto Pistol', ['AMMOlabel'] = 'ammopistolnormal', ["AMMOPRICE"] = 1, ["AMMOCOUNT"] = 100 },
-    [7] = { ['weapon'] = 'WEAPON_PISTOL_MAUSER', ["PRICE"] = 100, ['label'] = 'Mauser Pistol', ['AMMOlabel'] = 'ammopistolnormal', ["AMMOPRICE"] = 1, ["AMMOCOUNT"] = 100 },
+    [1] = { ['weapon'] = 'WEAPON_REVOLVER_CATTLEMAN', ["PRICE"] = 10, ['label'] = 'Cattleman Revolver', ['AMMOlabel'] = 'ammorevolvernormal', ["AMMOPRICE"] = 1},
+    [2] = { ['weapon'] = 'WEAPON_REVOLVER_DOUBLEACTION', ["PRICE"] = 20, ['label'] = 'Double-Action Revolver', ['AMMOlabel'] = 'ammorevolvernormal', ["AMMOPRICE"] = 1},
+    [3] = { ['weapon'] = 'WEAPON_REVOLVER_LEMAT', ["PRICE"] = 55, ['label'] = 'LeMat Revolver', ['AMMOlabel'] = 'ammorevolvernormal', ["AMMOPRICE"] = 1},
+    [4] = { ['weapon'] = 'WEAPON_REVOLVER_SCHOFIELD', ["PRICE"] = 30, ['label'] = 'Schofield Revolver', ['AMMOlabel'] = 'ammorevolvernormal', ["AMMOPRICE"] = 1},
+    [5] = { ['weapon'] = 'WEAPON_PISTOL_VOLCANIC', ["PRICE"] = 35, ['label'] = 'Volcanic Pistol', ['AMMOlabel'] = 'ammopistolnormal', ["AMMOPRICE"] = 1},
+    [6] = { ['weapon'] = 'WEAPON_PISTOL_SEMIAUTO', ["PRICE"] = 120, ['label'] = 'Semi-Auto Pistol', ['AMMOlabel'] = 'ammopistolnormal', ["AMMOPRICE"] = 1},
+    [7] = { ['weapon'] = 'WEAPON_PISTOL_MAUSER', ["PRICE"] = 100, ['label'] = 'Mauser Pistol', ['AMMOlabel'] = 'ammopistolnormal', ["AMMOPRICE"] = 1},
     
-    [8] = { ['weapon'] = 'WEAPON_REPEATER_CARBINE', ["PRICE"] = 45, ['label'] = 'Carbine Repeater', ['AMMOlabel'] = 'ammorepeaternormal', ["AMMOPRICE"] = 1, ["AMMOCOUNT"] = 100 },
-    [9] = { ['weapon'] = 'WEAPON_REPEATER_WINCHESTER', ["PRICE"] = 120, ['label'] = 'Lancaster Repeater', ['AMMOlabel'] = 'ammorepeaternormal', ["AMMOPRICE"] = 2, ["AMMOCOUNT"] = 100 }, 
-    [10] = { ['weapon'] = 'WEAPON_REPEATER_EVANS', ["PRICE"] = 170, ['label'] = 'Evans Repeater', ['AMMOlabel'] = 'ammorepeaternormal', ["AMMOPRICE"] = 2, ["AMMOCOUNT"] = 100 },
+    [8] = { ['weapon'] = 'WEAPON_REPEATER_CARBINE', ["PRICE"] = 45, ['label'] = 'Carbine Repeater', ['AMMOlabel'] = 'ammorepeaternormal', ["AMMOPRICE"] = 1},
+    [9] = { ['weapon'] = 'WEAPON_REPEATER_WINCHESTER', ["PRICE"] = 120, ['label'] = 'Lancaster Repeater', ['AMMOlabel'] = 'ammorepeaternormal', ["AMMOPRICE"] = 2}, 
+    [10] = { ['weapon'] = 'WEAPON_REPEATER_EVANS', ["PRICE"] = 170, ['label'] = 'Evans Repeater', ['AMMOlabel'] = 'ammorepeaternormal', ["AMMOPRICE"] = 2},
     
-    [11] = { ['weapon'] = 'WEAPON_RIFLE_VARMINT', ["PRICE"] = 35, ['label'] = 'Varmint Rifle', ['AMMOlabel'] = 'ammovarmint', ["AMMOPRICE"] = 1, ["AMMOCOUNT"] = 100 },
+    [11] = { ['weapon'] = 'WEAPON_RIFLE_VARMINT', ["PRICE"] = 35, ['label'] = 'Varmint Rifle', ['AMMOlabel'] = 'ammovarmint', ["AMMOPRICE"] = 1,},
     
-    [12] = { ['weapon'] = 'WEAPON_RIFLE_SPRINGFIELD', ["PRICE"] = 265, ['label'] = 'Springfield Rifle', ['AMMOlabel'] = 'ammoriflenormal', ["AMMOPRICE"] = 2, ["AMMOCOUNT"] = 100 },
-    [13] = { ['weapon'] = 'WEAPON_RIFLE_BOLTACTION', ["PRICE"] = 275, ['label'] = 'Bolt-Action Rifle', ['AMMOlabel'] = 'ammoriflenormal', ["AMMOPRICE"] = 2, ["AMMOCOUNT"] = 100 },
-    [14] = { ['weapon'] = 'WEAPON_SNIPERRIFLE_ROLLINGBLOCK', ["PRICE"] = 290, ['label'] = 'Rolling Block Rifle', ['AMMOlabel'] = 'ammoriflenormal', ["AMMOPRICE"] = 2, ["AMMOCOUNT"] = 100 },
-    [15] = { ['weapon'] = 'WEAPON_SNIPERRIFLE_CARCANO', ["PRICE"] = 310, ['label'] = 'Carcano Rifle', ['AMMOlabel'] = 'ammoriflenormal', ["AMMOPRICE"] = 2, ["AMMOCOUNT"] = 100 },
+    [12] = { ['weapon'] = 'WEAPON_RIFLE_SPRINGFIELD', ["PRICE"] = 265, ['label'] = 'Springfield Rifle', ['AMMOlabel'] = 'ammoriflenormal', ["AMMOPRICE"] = 2},
+    [13] = { ['weapon'] = 'WEAPON_RIFLE_BOLTACTION', ["PRICE"] = 275, ['label'] = 'Bolt-Action Rifle', ['AMMOlabel'] = 'ammoriflenormal', ["AMMOPRICE"] = 2},
+    [14] = { ['weapon'] = 'WEAPON_SNIPERRIFLE_ROLLINGBLOCK', ["PRICE"] = 290, ['label'] = 'Rolling Block Rifle', ['AMMOlabel'] = 'ammoriflenormal', ["AMMOPRICE"] = 2},
+    [15] = { ['weapon'] = 'WEAPON_SNIPERRIFLE_CARCANO', ["PRICE"] = 310, ['label'] = 'Carcano Rifle', ['AMMOlabel'] = 'ammoriflenormal', ["AMMOPRICE"] = 2},
     
-    [16] = { ['weapon'] = 'WEAPON_SHOTGUN_SAWEDOFF', ["PRICE"] = 140, ['label'] = 'Sawed-Off Shotgun', ['AMMOlabel'] = 'ammoshotgunnormal', ["AMMOPRICE"] = 2, ["AMMOCOUNT"] = 100 },
-    [17] = { ['weapon'] = 'WEAPON_SHOTGUN_DOUBLEBARREL', ["PRICE"] = 65, ['label'] = 'Double-Barrel Shotgun', ['AMMOlabel'] = 'ammoshotgunnormal', ["AMMOPRICE"] = 2, ["AMMOCOUNT"] = 100 },
-    [18] = { ['weapon'] = 'WEAPON_SHOTGUN_PUMP', ["PRICE"] = 80, ['label'] = 'Pump-Action Shotgun', ['AMMOlabel'] = 'ammoshotgunnormal', ["AMMOPRICE"] = 2, ["AMMOCOUNT"] = 100 },
-    [19] = { ['weapon'] = 'WEAPON_SHOTGUN_REPEATING', ["PRICE"] = 170, ['label'] = 'Repeating Shotgun', ['AMMOlabel'] = 'ammoshotgunnormal', ["AMMOPRICE"] = 2, ["AMMOCOUNT"] = 100 },
-    [20] = { ['weapon'] = 'WEAPON_SHOTGUN_SEMIAUTO', ["PRICE"] = 180, ['label'] = 'Semi-Auto Shotgun', ['AMMOlabel'] = 'ammoshotgunnormal', ["AMMOPRICE"] = 2, ["AMMOCOUNT"] = 100 },
+    [16] = { ['weapon'] = 'WEAPON_SHOTGUN_SAWEDOFF', ["PRICE"] = 140, ['label'] = 'Sawed-Off Shotgun', ['AMMOlabel'] = 'ammoshotgunnormal', ["AMMOPRICE"] = 2},
+    [17] = { ['weapon'] = 'WEAPON_SHOTGUN_DOUBLEBARREL', ["PRICE"] = 65, ['label'] = 'Double-Barrel Shotgun', ['AMMOlabel'] = 'ammoshotgunnormal', ["AMMOPRICE"] = 2},
+    [18] = { ['weapon'] = 'WEAPON_SHOTGUN_PUMP', ["PRICE"] = 80, ['label'] = 'Pump-Action Shotgun', ['AMMOlabel'] = 'ammoshotgunnormal', ["AMMOPRICE"] = 2},
+    [19] = { ['weapon'] = 'WEAPON_SHOTGUN_REPEATING', ["PRICE"] = 170, ['label'] = 'Repeating Shotgun', ['AMMOlabel'] = 'ammoshotgunnormal', ["AMMOPRICE"] = 2},
+    [20] = { ['weapon'] = 'WEAPON_SHOTGUN_SEMIAUTO', ["PRICE"] = 180, ['label'] = 'Semi-Auto Shotgun', ['AMMOlabel'] = 'ammoshotgunnormal', ["AMMOPRICE"] = 2},
     
     [21] = { ['weapon'] = 'WEAPON_BOW', ["PRICE"] = 35, ['label'] = 'Bow', ['AMMOlabel'] = 'arrow', ["AMMOPRICE"] = 1, ["AMMOCOUNT"] = 20 },
     [22] = { ['weapon'] = 'WEAPON_LASSO', ["PRICE"] = 10, ['label'] = 'Lasso', ['AMMOlabel'] = 'none', ["AMMOPRICE"] = 0},
@@ -30,11 +30,11 @@ local weapons = {
     [24] = { ['weapon'] = 'WEAPON_MELEE_LANTERN', ["PRICE"] = 8, ['label'] = 'Lantern', ['AMMOlabel'] = 'none', ["AMMOPRICE"] = 0},
     [25] = { ['weapon'] = 'WEAPON_MELEE_HATCHET', ["PRICE"] = 10, ['label'] = 'Hatchet', ['AMMOlabel'] = 'none', ["AMMOPRICE"] = 0},
     [26] = { ['weapon'] = 'WEAPON_MELEE_KNIFE', ["PRICE"] = 7, ['label'] = 'Knife', ['AMMOlabel'] = 'none', ["AMMOPRICE"] = 0},
-    [27] = { ['weapon'] = 'WEAPON_THROWN_THROWING_KNIVES', ["PRICE"] = 15, ['label'] = 'Throwing Knives', ['AMMOlabel'] = 'throwing knives', ["AMMOPRICE"] = 5, ["AMMOCOUNT"] = 5 },
+    [27] = { ['weapon'] = 'WEAPON_THROWN_THROWING_KNIVES', ["PRICE"] = 15, ['label'] = 'Throwing Knives', ['AMMOlabel'] = 'throwing knives', ["AMMOPRICE"] = 5},
     [28] = { ['weapon'] = 'WEAPON_MELEE_MACHETE', ["PRICE"] = 12, ['label'] = 'Machete', ['AMMOlabel'] = 'none', ["AMMOPRICE"] = 0},
-    [29] = { ['weapon'] = 'WEAPON_THROWN_TOMAHAWK', ["PRICE"] = 20, ['label'] = 'Tomahawk', ['AMMOlabel'] = 'tomahawk', ["AMMOPRICE"] = 5, ["AMMOCOUNT"] = 5 },
-    [30] = { ['weapon'] = 'WEAPON_THROWN_DYNAMITE', ["PRICE"] = 30, ['label'] = 'Dynamite', ['AMMOlabel'] = 'dynamite', ["AMMOPRICE"] = 10, ["AMMOCOUNT"] = 5 },
-    [31] = { ['weapon'] = 'WEAPON_THROWN_MOLOTOV', ["PRICE"] = 25, ['label'] = 'Molotov', ['AMMOlabel'] = 'molotov', ["AMMOPRICE"] = 8, ["AMMOCOUNT"] = 5 },
+    [29] = { ['weapon'] = 'WEAPON_THROWN_TOMAHAWK', ["PRICE"] = 20, ['label'] = 'Tomahawk', ['AMMOlabel'] = 'tomahawk', ["AMMOPRICE"] = 5},
+    [30] = { ['weapon'] = 'WEAPON_THROWN_DYNAMITE', ["PRICE"] = 30, ['label'] = 'Dynamite', ['AMMOlabel'] = 'dynamite', ["AMMOPRICE"] = 10},
+    [31] = { ['weapon'] = 'WEAPON_THROWN_MOLOTOV', ["PRICE"] = 25, ['label'] = 'Molotov', ['AMMOlabel'] = 'molotov', ["AMMOPRICE"] = 8},
 }
 
 local securecode = math.random(111111,9999999)
@@ -77,14 +77,14 @@ AddEventHandler('gunCatalogue:Purchase', function(data, code)
                 elseif data.isammo == 1 then
                     if money >= v.AMMOPRICE then
 
-                        local canCarry = exports.vorp_inventory:canCarryItem(source, v.AMMOlabel, v.AMMOCOUNT)
+                        local canCarry = exports.vorp_inventory:canCarryItem(source, v.AMMOlabel, 1)
                         if not canCarry then
                             return Core.NotifyRightTip(source, "Can't carry any more items!", 3000)
                         end
                         
                         Character.removeCurrency(0, v.AMMOPRICE)
                         TriggerClientEvent('gunCatalogue:giveammo', User, v.weapon, securecode)
-                        exports.vorp_inventory:addBullets(source, v.AMMOlabel, v.AMMOCOUNT)
+                        exports.vorp_inventory:addItem(source, v.AMMOlabel, 1)
                         Core.NotifyRightTip(User, "You have purchased " .. v.AMMOlabel, 4000)
                         PlaySoundFrontend("PURCHASE", "Ledger_Sounds", true, 0)
                     else

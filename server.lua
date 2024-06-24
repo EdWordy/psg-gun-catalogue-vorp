1c786e216f96e3f9139a77bd25659b6f5d1283d7
EdWordy_v2.0 <taylorjordanedwards@gmail.com>
2024-06-24 13:06:23 -0600

    Update 5
    -Fix ammo purchasing

--- server.lua
+++ server.lua
@@ -1,28 +1,28 @@
 local weapons = {
-    [1] = { ['weapon'] = 'WEAPON_REVOLVER_CATTLEMAN', ["PRICE"] = 10, ['label'] = 'Cattleman Revolver', ['AMMOlabel'] = 'revolver ammo', ["AMMOPRICE"] = 1, ["AMMOCOUNT"] = 100 },
-    [2] = { ['weapon'] = 'WEAPON_REVOLVER_DOUBLEACTION', ["PRICE"] = 20, ['label'] = 'Double-Action Revolver', ['AMMOlabel'] = 'revolver ammo', ["AMMOPRICE"] = 1, ["AMMOCOUNT"] = 100 },
-    [3] = { ['weapon'] = 'WEAPON_REVOLVER_LEMAT', ["PRICE"] = 55, ['label'] = 'LeMat Revolver', ['AMMOlabel'] = 'revolver ammo', ["AMMOPRICE"] = 1, ["AMMOCOUNT"] = 100 },
-    [4] = { ['weapon'] = 'WEAPON_REVOLVER_SCHOFIELD', ["PRICE"] = 30, ['label'] = 'Schofield Revolver', ['AMMOlabel'] = 'revolver ammo', ["AMMOPRICE"] = 1, ["AMMOCOUNT"] = 100 },
-    [5] = { ['weapon'] = 'WEAPON_PISTOL_VOLCANIC', ["PRICE"] = 35, ['label'] = 'Volcanic Pistol', ['AMMOlabel'] = 'pistol ammo', ["AMMOPRICE"] = 1, ["AMMOCOUNT"] = 100 },
-    [6] = { ['weapon'] = 'WEAPON_PISTOL_SEMIAUTO', ["PRICE"] = 120, ['label'] = 'Semi-Auto Pistol', ['AMMOlabel'] = 'pistol ammo', ["AMMOPRICE"] = 1, ["AMMOCOUNT"] = 100 },
-    [7] = { ['weapon'] = 'WEAPON_PISTOL_MAUSER', ["PRICE"] = 100, ['label'] = 'Mauser Pistol', ['AMMOlabel'] = 'pistol ammo', ["AMMOPRICE"] = 1, ["AMMOCOUNT"] = 100 },
+    [1] = { ['weapon'] = 'WEAPON_REVOLVER_CATTLEMAN', ["PRICE"] = 10, ['label'] = 'Cattleman Revolver', ['AMMOlabel'] = 'ammorevolvernormal', ["AMMOPRICE"] = 1, ["AMMOCOUNT"] = 100 },
+    [2] = { ['weapon'] = 'WEAPON_REVOLVER_DOUBLEACTION', ["PRICE"] = 20, ['label'] = 'Double-Action Revolver', ['AMMOlabel'] = 'ammorevolvernormal', ["AMMOPRICE"] = 1, ["AMMOCOUNT"] = 100 },
+    [3] = { ['weapon'] = 'WEAPON_REVOLVER_LEMAT', ["PRICE"] = 55, ['label'] = 'LeMat Revolver', ['AMMOlabel'] = 'ammorevolvernormal', ["AMMOPRICE"] = 1, ["AMMOCOUNT"] = 100 },
+    [4] = { ['weapon'] = 'WEAPON_REVOLVER_SCHOFIELD', ["PRICE"] = 30, ['label'] = 'Schofield Revolver', ['AMMOlabel'] = 'ammorevolvernormal', ["AMMOPRICE"] = 1, ["AMMOCOUNT"] = 100 },
+    [5] = { ['weapon'] = 'WEAPON_PISTOL_VOLCANIC', ["PRICE"] = 35, ['label'] = 'Volcanic Pistol', ['AMMOlabel'] = 'ammopistolnormal', ["AMMOPRICE"] = 1, ["AMMOCOUNT"] = 100 },
+    [6] = { ['weapon'] = 'WEAPON_PISTOL_SEMIAUTO', ["PRICE"] = 120, ['label'] = 'Semi-Auto Pistol', ['AMMOlabel'] = 'ammopistolnormal', ["AMMOPRICE"] = 1, ["AMMOCOUNT"] = 100 },
+    [7] = { ['weapon'] = 'WEAPON_PISTOL_MAUSER', ["PRICE"] = 100, ['label'] = 'Mauser Pistol', ['AMMOlabel'] = 'ammopistolnormal', ["AMMOPRICE"] = 1, ["AMMOCOUNT"] = 100 },
     
-    [8] = { ['weapon'] = 'WEAPON_REPEATER_CARBINE', ["PRICE"] = 45, ['label'] = 'Carbine Repeater', ['AMMOlabel'] = 'repeater ammo', ["AMMOPRICE"] = 1, ["AMMOCOUNT"] = 100 },
-    [9] = { ['weapon'] = 'WEAPON_REPEATER_WINCHESTER', ["PRICE"] = 120, ['label'] = 'Lancaster Repeater', ['AMMOlabel'] = 'repeater ammo', ["AMMOPRICE"] = 2, ["AMMOCOUNT"] = 100 }, 
-    [10] = { ['weapon'] = 'WEAPON_REPEATER_EVANS', ["PRICE"] = 170, ['label'] = 'Evans Repeater', ['AMMOlabel'] = 'repeater ammo', ["AMMOPRICE"] = 2, ["AMMOCOUNT"] = 100 },
+    [8] = { ['weapon'] = 'WEAPON_REPEATER_CARBINE', ["PRICE"] = 45, ['label'] = 'Carbine Repeater', ['AMMOlabel'] = 'ammorepeaternormal', ["AMMOPRICE"] = 1, ["AMMOCOUNT"] = 100 },
+    [9] = { ['weapon'] = 'WEAPON_REPEATER_WINCHESTER', ["PRICE"] = 120, ['label'] = 'Lancaster Repeater', ['AMMOlabel'] = 'ammorepeaternormal', ["AMMOPRICE"] = 2, ["AMMOCOUNT"] = 100 }, 
+    [10] = { ['weapon'] = 'WEAPON_REPEATER_EVANS', ["PRICE"] = 170, ['label'] = 'Evans Repeater', ['AMMOlabel'] = 'ammorepeaternormal', ["AMMOPRICE"] = 2, ["AMMOCOUNT"] = 100 },
     
-    [11] = { ['weapon'] = 'WEAPON_RIFLE_VARMINT', ["PRICE"] = 35, ['label'] = 'Varmint Rifle', ['AMMOlabel'] = 'rifle ammo', ["AMMOPRICE"] = 1, ["AMMOCOUNT"] = 100 },
+    [11] = { ['weapon'] = 'WEAPON_RIFLE_VARMINT', ["PRICE"] = 35, ['label'] = 'Varmint Rifle', ['AMMOlabel'] = 'ammovarmint', ["AMMOPRICE"] = 1, ["AMMOCOUNT"] = 100 },
     
-    [12] = { ['weapon'] = 'WEAPON_RIFLE_SPRINGFIELD', ["PRICE"] = 265, ['label'] = 'Springfield Rifle', ['AMMOlabel'] = 'rifle ammo', ["AMMOPRICE"] = 2, ["AMMOCOUNT"] = 100 },
-    [13] = { ['weapon'] = 'WEAPON_RIFLE_BOLTACTION', ["PRICE"] = 275, ['label'] = 'Bolt-Action Rifle', ['AMMOlabel'] = 'rifle ammo', ["AMMOPRICE"] = 2, ["AMMOCOUNT"] = 100 },
-    [14] = { ['weapon'] = 'WEAPON_SNIPERRIFLE_ROLLINGBLOCK', ["PRICE"] = 290, ['label'] = 'Rolling Block Rifle', ['AMMOlabel'] = 'sniper ammo', ["AMMOPRICE"] = 2, ["AMMOCOUNT"] = 100 },
-    [15] = { ['weapon'] = 'WEAPON_SNIPERRIFLE_CARCANO', ["PRICE"] = 310, ['label'] = 'Carcano Rifle', ['AMMOlabel'] = 'sniper ammo', ["AMMOPRICE"] = 2, ["AMMOCOUNT"] = 100 },
+    [12] = { ['weapon'] = 'WEAPON_RIFLE_SPRINGFIELD', ["PRICE"] = 265, ['label'] = 'Springfield Rifle', ['AMMOlabel'] = 'ammoriflenormal', ["AMMOPRICE"] = 2, ["AMMOCOUNT"] = 100 },
+    [13] = { ['weapon'] = 'WEAPON_RIFLE_BOLTACTION', ["PRICE"] = 275, ['label'] = 'Bolt-Action Rifle', ['AMMOlabel'] = 'ammoriflenormal', ["AMMOPRICE"] = 2, ["AMMOCOUNT"] = 100 },
+    [14] = { ['weapon'] = 'WEAPON_SNIPERRIFLE_ROLLINGBLOCK', ["PRICE"] = 290, ['label'] = 'Rolling Block Rifle', ['AMMOlabel'] = 'ammoriflenormal', ["AMMOPRICE"] = 2, ["AMMOCOUNT"] = 100 },
+    [15] = { ['weapon'] = 'WEAPON_SNIPERRIFLE_CARCANO', ["PRICE"] = 310, ['label'] = 'Carcano Rifle', ['AMMOlabel'] = 'ammoriflenormal', ["AMMOPRICE"] = 2, ["AMMOCOUNT"] = 100 },
     
-    [16] = { ['weapon'] = 'WEAPON_SHOTGUN_SAWEDOFF', ["PRICE"] = 140, ['label'] = 'Sawed-Off Shotgun', ['AMMOlabel'] = 'shotgun ammo', ["AMMOPRICE"] = 2, ["AMMOCOUNT"] = 100 },
-    [17] = { ['weapon'] = 'WEAPON_SHOTGUN_DOUBLEBARREL', ["PRICE"] = 65, ['label'] = 'Double-Barrel Shotgun', ['AMMOlabel'] = 'shotgun ammo', ["AMMOPRICE"] = 2, ["AMMOCOUNT"] = 100 },
-    [18] = { ['weapon'] = 'WEAPON_SHOTGUN_PUMP', ["PRICE"] = 80, ['label'] = 'Pump-Action Shotgun', ['AMMOlabel'] = 'shotgun ammo', ["AMMOPRICE"] = 2, ["AMMOCOUNT"] = 100 },
-    [19] = { ['weapon'] = 'WEAPON_SHOTGUN_REPEATING', ["PRICE"] = 170, ['label'] = 'Repeating Shotgun', ['AMMOlabel'] = 'shotgun ammo', ["AMMOPRICE"] = 2, ["AMMOCOUNT"] = 100 },
-    [20] = { ['weapon'] = 'WEAPON_SHOTGUN_SEMIAUTO', ["PRICE"] = 180, ['label'] = 'Semi-Auto Shotgun', ['AMMOlabel'] = 'shotgun ammo', ["AMMOPRICE"] = 2, ["AMMOCOUNT"] = 100 },
+    [16] = { ['weapon'] = 'WEAPON_SHOTGUN_SAWEDOFF', ["PRICE"] = 140, ['label'] = 'Sawed-Off Shotgun', ['AMMOlabel'] = 'ammoshotgunnormal', ["AMMOPRICE"] = 2, ["AMMOCOUNT"] = 100 },
+    [17] = { ['weapon'] = 'WEAPON_SHOTGUN_DOUBLEBARREL', ["PRICE"] = 65, ['label'] = 'Double-Barrel Shotgun', ['AMMOlabel'] = 'ammoshotgunnormal', ["AMMOPRICE"] = 2, ["AMMOCOUNT"] = 100 },
+    [18] = { ['weapon'] = 'WEAPON_SHOTGUN_PUMP', ["PRICE"] = 80, ['label'] = 'Pump-Action Shotgun', ['AMMOlabel'] = 'ammoshotgunnormal', ["AMMOPRICE"] = 2, ["AMMOCOUNT"] = 100 },
+    [19] = { ['weapon'] = 'WEAPON_SHOTGUN_REPEATING', ["PRICE"] = 170, ['label'] = 'Repeating Shotgun', ['AMMOlabel'] = 'ammoshotgunnormal', ["AMMOPRICE"] = 2, ["AMMOCOUNT"] = 100 },
+    [20] = { ['weapon'] = 'WEAPON_SHOTGUN_SEMIAUTO', ["PRICE"] = 180, ['label'] = 'Semi-Auto Shotgun', ['AMMOlabel'] = 'ammoshotgunnormal', ["AMMOPRICE"] = 2, ["AMMOCOUNT"] = 100 },
     
     [21] = { ['weapon'] = 'WEAPON_BOW', ["PRICE"] = 35, ['label'] = 'Bow', ['AMMOlabel'] = 'arrow', ["AMMOPRICE"] = 1, ["AMMOCOUNT"] = 20 },
     [22] = { ['weapon'] = 'WEAPON_LASSO', ["PRICE"] = 10, ['label'] = 'Lasso', ['AMMOlabel'] = 'none', ["AMMOPRICE"] = 0},
@@ -106,23 +106,6 @@ AddEventHandler('gunCatalogue:getCode', function()
     local _source = source
     TriggerClientEvent('gunCatalogue:SendCode', _source, securecode)
 end)
-
-local function registerUsableItem(itemName, weaponName)
-    exports.vorp_inventory:registerUsableItem(itemName, function(data)
-        local _source = data.source
-        TriggerClientEvent('gunCatalogue:giveammo', _source, weaponName, securecode)
-        exports.vorp_inventory:subItem(_source, itemName, 1)
-    end)
-end
-
-registerUsableItem("ammorevolvernormal", "WEAPON_REVOLVER_CATTLEMAN")
-registerUsableItem("ammopistolnormal", "WEAPON_PISTOL_MAUSER")
-registerUsableItem("ammovarmint", "WEAPON_RIFLE_VARMINT")
-registerUsableItem("ammoriflenormal", "WEAPON_RIFLE_BOLTACTION")
-registerUsableItem("ammoshotgunnormal", "WEAPON_SHOTGUN_DOUBLEBARREL")
-registerUsableItem("ammorepeaternormal", "WEAPON_REPEATER_EVANS")
-registerUsableItem("ammoriflenormal", "WEAPON_SNIPERRIFLE_CARCANO")
-registerUsableItem("ammoarrownormal", "WEAPON_BOW")
 
 -- Print contents of `tbl`, with indentation.
 -- `indent` sets the initial level of indentation.

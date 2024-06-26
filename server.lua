cb417c9b1b5f173a1c3a26a95e82fcc2156b14b8
EdWordy_v2.0 <taylorjordanedwards@gmail.com>
2024-06-25 19:07:05 -0600

    Update 15
    -fixes for various issues including tooltips and nil values

--- server.lua
+++ server.lua
@@ -1,13 +1,12 @@
 -- do not touch this
 local securecode = math.random(111111,9999999)
 
--- Initialize VORP API
-local Core = exports.vorp_core:GetCore()
+local VORPcore = exports.vorp_core:GetCore()
 
 RegisterNetEvent('gunCatalogue:Purchase')
 AddEventHandler('gunCatalogue:Purchase', function(data, code)
 
-    local User = Core.getUser(source)
+    local User = VORPcore.getUser(source)
 
     if User then
         local Character = User.getUsedCharacter
@@ -20,42 +19,42 @@ AddEventHandler('gunCatalogue:Purchase', function(data, code)
                     if money >= v.price then
                         local canCarry = exports.vorp_inventory:canCarryWeapons(source, 1, nil, v.weapon)
                         if not canCarry then
-                            Core.NotifyRightTip(source, "Can't carry any more weapons!", 3000)
+                            VORPcore.NotifyRightTip(source, "Can't carry any more weapons!", 3000)
                             return
                         end
                         Character.removeCurrency(0, v.price)
                         local ammo = { ["nothing"] = 0 }
                         local components = { ["nothing"] = 0 }  
                         exports.vorp_inventory:createWeapon(source, v.weapon, ammo, components)
-                        Core.NotifyRightTip(source, "You have purchased a " .. v.label, 4000)
+                        VORPcore.NotifyRightTip(source, "You have purchased a " .. v.label, 4000)
                     else
-                        Core.NotifyRight(source, "You do not have enough money", 3000)
+                        VORPcore.NotifyRightTip(source, "You do not have enough money", 3000)
                     end
                     break
                 elseif data.isammo == 1 then
                     if money >= v.ammoprice then
                         local canCarry = exports.vorp_inventory:canCarryItem(source, v.ammo, 1)
                         if not canCarry then
-                            return Core.NotifyRightTip(source, "Can't carry any more items!", 3000)
+                            return VORPcore.NotifyRightTip(source, "Can't carry any more items!", 3000)
                         end
                         if v.ammo ~= 'none' then
                             Character.removeCurrency(0, v.ammoprice)
                             TriggerClientEvent('gunCatalogue:giveAmmo', source, v.weapon, securecode)
                             exports.vorp_inventory:addItem(source, v.ammo, 1)
-                            Core.NotifyRightTip(source, "You have purchased " .. v.ammolabel, 4000)
+                            VORPcore.NotifyRightTip(source, "You have purchased " .. v.ammolabel, 4000)
                             TriggerClientEvent('gunCatalogue:playSoundPurchase', source)
                         else
-                            Core.NotifyRight(source, "This weapon has no ammo available", 3000)
+                            VORPcore.NotifyRightTip(source, "This weapon has no ammo available", 3000)
                         end
                     else
-                        Core.NotifyRight(source, "You do not have enough money", 3000)
+                        VORPcore.NotifyRightTip(source, "You do not have enough money", 3000)
                     end
                     break
                 end
             end
             end
         else
-            Core.NotifyRight(source, "ERROR: Invalid code", 3000)
+            VORPcore.NotifyRightTip(source, "ERROR: Invalid code", 3000)
         end
     else
         print("ERROR: getUser returned nil for source: " .. tostring(source))
@@ -64,7 +63,6 @@ end)
 
 RegisterServerEvent('gunCatalogue:getCode')
 AddEventHandler('gunCatalogue:getCode', function()
-    local _source = source
     TriggerClientEvent('gunCatalogue:sendCode', source, securecode)
 end)
 
